5c46a9bf365a2e82341e38af63c5b57fb573a843
16Komang <suputrakomang22@gmail.com>
2022-03-20 15:53:02 +0700

    prakrikum4 bagiian 2

---
+++ prak5/app/Http/Controllers/PegawaiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pegawai;
+
+class PegawaiController extends Controller
+{
+    public function index() { 
+ 
+        // mengambil data pegawai yang pertama
+        $pegawai = Pegawai::first(); 
+     
+        // mengirim data pegawai ke view pegawai 
+        return view('pegawai', ['pegawai' => $pegawai]); 
+     
+    }
+}
